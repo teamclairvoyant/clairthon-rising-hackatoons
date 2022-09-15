10547e8d2821c69856ef1a3ae4ff69881e4e6b6e
Yashi <yashi.songara@clairvoyantsoft.com>
2022-09-15 15:44:19 +0530

    CRH-41:(chore) Moved the folder

--- src/app/modules/candidate-registration/candidate-registration-routing.module.ts
+++ src/app/modules/candidate-registration/candidate-registration-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 
 const routes: Routes = [
   {
@@ -11,6 +11,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CandidateRegistrationRoutingModule { }
+export class CandidateRegistrationRoutingModule {}
--- src/app/modules/candidate-registration/candidate-registration.module.ts
+++ src/app/modules/candidate-registration/candidate-registration.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CandidateRegistrationRoutingModule } from './candidate-registration-routing.module';
-import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
-
 @NgModule({
-  declarations: [
-    RegistrationFormComponent
-  ],
+  declarations: [RegistrationFormComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     NgMultiSelectDropDownModule.forRoot(),
-    CandidateRegistrationRoutingModule
-  ]
+    CandidateRegistrationRoutingModule,
+  ],
 })
-export class CandidateRegistrationModule { }
+export class CandidateRegistrationModule {}
