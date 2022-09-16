5eba18a331a4bf124a80da60f04235ad8c1f9609
Yashi <yashi.songara@clairvoyantsoft.com>
2022-09-16 22:09:22 +0530

    CRH-41:(test) Fixed failing unit test cases

--- src/app/modules/question-bank/components/upload-questions/upload-questions.component.spec.ts
+++ src/app/modules/question-bank/components/upload-questions/upload-questions.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UploadQuestionsComponent } from './upload-questions.component';
 
 describe('UploadQuestionsComponent', () => {
@@ -8,9 +8,10 @@ describe('UploadQuestionsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UploadQuestionsComponent ]
-    })
-    .compileComponents();
+      declarations: [UploadQuestionsComponent],
+      imports: [FormsModule, ReactiveFormsModule],
+      providers: [FormBuilder],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UploadQuestionsComponent);
     component = fixture.componentInstance;
@@ -19,5 +20,11 @@ describe('UploadQuestionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create form on ngOnInit', () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.uploadQuestionForm).toBeTruthy();
   });
 });
