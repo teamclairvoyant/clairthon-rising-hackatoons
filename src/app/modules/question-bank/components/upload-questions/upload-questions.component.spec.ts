f7875a4660464958bcdfaca624505770d5808b07
Yashi <yashi.songara@clairvoyantsoft.com>
2022-09-22 23:00:00 +0530

    CRH-30:(test) fix unit test cases

--- src/app/modules/question-bank/components/upload-questions/upload-questions.component.spec.ts
+++ src/app/modules/question-bank/components/upload-questions/upload-questions.component.spec.ts
@@ -1,5 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { QuestionBankService } from '../../services/question-bank.service';
 import { UploadQuestionsComponent } from './upload-questions.component';
 
 describe('UploadQuestionsComponent', () => {
@@ -9,8 +12,8 @@ describe('UploadQuestionsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UploadQuestionsComponent],
-      imports: [FormsModule, ReactiveFormsModule],
-      providers: [FormBuilder],
+      imports: [FormsModule, ReactiveFormsModule, HttpClientTestingModule, ToastrModule.forRoot()],
+      providers: [FormBuilder, QuestionBankService, ToastrService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UploadQuestionsComponent);
