0c4546b9f2ce685153e0e059c5be25286b47b02a
Sudesh Patil <95691336+cv-sudesh-patil@users.noreply.github.com>
2022-09-27 17:27:27 +0530

    CRH-60(feat): UI navigation flow for coding test (#14)
    * CRH-60(feat): UI navigation flow for coding test
    
    * CRH-60(feat): UI navigation flow for coding test
    
    * CRH-60(feat): UI navigation flow for coding test and resolved failed test case
    
    * Resolved PR review comments

--- src/app/modules/coding-test/general-instructions/general-instructions.component.ts
+++ src/app/modules/coding-test/general-instructions/general-instructions.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CandidateRegistrationService } from '../../candidate-registration/services/candidate-registration.service';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-general-instructions',
   templateUrl: './general-instructions.component.html',
-  styleUrls: ['./general-instructions.component.scss',],
+  styleUrls: ['./general-instructions.component.scss'],
 })
-export class GeneralInstructionsModule {
+export class GeneralInstructionsComponent implements OnInit {
+  /**
+   * Candidate id to fetch test details
+   */
+  candidateId = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private candidateRegistrationSer: CandidateRegistrationService,
+    private toastr: ToastrService,
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.candidateId = params['id'] as string;
+      this.authService.hideHeader = true;
+      this.getCodingTest();
+    });
+  }
+
+  private getCodingTest(): void {
+    this.candidateRegistrationSer.generateTestLink(this.candidateId).subscribe(
+      (response: any) => {
+        if (response?.statusCode) {
+          console.log('response: ', JSON.parse(response.body));
+        }
+      },
+      (_error) => {
+        this.toastr.error('Something went wrong, Please try again!!');
+      },
+    );
+  }
 }
