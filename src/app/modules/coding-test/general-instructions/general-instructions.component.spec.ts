7076ed5e9473a2d26f0638db8b4ea7920f8b68fc
Sudesh.patil <sudesh.patil@clairvoyantsoft.com>
2022-09-27 16:20:22 +0530

    CRH-60(feat): UI navigation flow for coding test

--- src/app/modules/coding-test/general-instructions/general-instructions.component.spec.ts
+++ src/app/modules/coding-test/general-instructions/general-instructions.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { GeneralInstructionsComponent } from './general-instructions.component';
 
 describe('GeneralInstructionsComponent', () => {
@@ -8,6 +10,7 @@ describe('GeneralInstructionsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [GeneralInstructionsComponent],
+      imports: [RouterModule, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GeneralInstructionsComponent);
