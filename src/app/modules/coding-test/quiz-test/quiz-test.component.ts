c911f5ed61bcc75edc919ff4cf5d202c0f037419
Aakansha <aakansha.choudhary@clairvoyantsoft.com>
2022-09-27 18:33:01 +0530

    crh-16(feat): WIP

---
+++ src/app/modules/coding-test/quiz-test/quiz-test.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-test',
+  templateUrl: './quiz-test.component.html',
+  styleUrls: ['./quiz-test.component.scss'],
+})
+export class QuizTestComponent {}
