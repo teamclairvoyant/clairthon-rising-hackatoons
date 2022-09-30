c7779b2c0b1e82d5f62c27a320c5fcf30397182b
Yashi <yashi.songara@clairvoyantsoft.com>
2022-09-30 11:26:02 +0530

    CRH-24:(test) Fixed failing test cases

--- src/app/modules/candidate-registration/components/result/result.component.spec.ts
+++ src/app/modules/candidate-registration/components/result/result.component.spec.ts
@@ -1,5 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { ResultComponent } from './result.component';
 
 describe('ResultComponent', () => {
@@ -8,9 +10,23 @@ describe('ResultComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResultComponent ]
-    })
-    .compileComponents();
+      declarations: [ResultComponent],
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ candidateId: '5e3a3d123e4f11ed94edaa68deeeadbe' }),
+            snapshot: {},
+            queryParams: {
+              subscribe() {
+                return of({});
+              },
+            },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ResultComponent);
     component = fixture.componentInstance;
